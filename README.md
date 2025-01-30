3e6dfcc91eab506bf53d5b148228250b64713c56
javega33 <javega.qro@gmail.com>
2025-01-30 05:27:31 -0600

    new file:   .DS_Store 	modified:   README.md 	new file:   app.py 	new file:   notebook/EDA.ipynb 	new file:   requirements.txt 	new file:   streamlit/config.toml 	new file:   vehicles_us.csv

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Sp7_project
  Proyecto del Spint 7 Herramientas de Software
+
+ * Se crea el programa app.py, el cual nos muestra una pagina web con dos graficos: 
+    G1: Histograma de los precios de los autos con los que cuenta la Agencia. Aqui podemos los precios de los autos que tiene una agencia. Es útil para entender cómo se distribuyen los precios, identificar patrones o tendencias, y ver si hay concentraciones o irregularidades en los datos.
+
+    G2: Tenemos una disperción de los tipos de autos con los que cuenta una agencia. Nos muestra a la variedad o diversidad de los modelos y categorías de autos que la agencia tiene en su inventario. Esta dispersión puede ser vista desde varios puntos de vista: como sedanes, SUVs, camionetas, deportivos, etc. 
---
+++ app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('/Users/javega/Documents/bhi/manuales/python/TripleTen/Sprint7/proyecto/Sp7_project-1/vehicles_us.csv') # leer los datos
+#hist_button = st.button('Construir histograma') # crear un botón
+#disp_button = st.button('Construir Dispersión') # crear un botón
+
+# crear una casillas de verificación
+build_histogram = st.checkbox('Construir un histograma')
+build_disp = st.checkbox('Construir Dispersión') 
+
+#print(car_data.head(10))
+
+st.header('Analisis de Venta de Coches')
+       
+if build_histogram: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma el rango de precios de los autos con los que cuenta la agencia')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x='price')
+       
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_disp: # al hacer clic en el botón
+    #Filtramos informacion, contamos los tipos de coches que actualmente cuenta la agencia
+    types_car = car_data.groupby(by='type')['price'].agg('count')
+    types_car = types_car.reset_index()
+    
+    # escribir un mensaje
+    st.write('Creación de un Gráfico de Dispersión para ver los diferentes tipos de autos con los que cuenta la agencia.')
+    
+            
+    # crear un dispersion
+    fig = px.bar(types_car,x='type', y='price')
+       
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
